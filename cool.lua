cf67217cffbb0b716de73e4356fbab7c18d6c8d7
Unhaunting <109845166+Unhaunting@users.noreply.github.com>
2022-07-23 03:25:22 -0400

    Update cool.lua

--- cool.lua
+++ cool.lua
@@ -140,24 +140,25 @@ RunService.RenderStepped:Connect(function() -- Aiming
     end
 end)
 
-if Obscuring == false and ((Closest[1] ~= nil and DistanceFromCenter < Closest[1]) or Closest[1] == nil) then
-                                    if Closest[1] == nil or (DistanceFromCenter < Closest[1]) then
-                                        local Prediction = Vector3.new(0, 0, 0)
-                                        if MovementPrediction == true then
-                                            Prediction = PlayerRoot.Velocity * (MovementPredictionStrength / 10) * (Client.Character.Head.Position - PlayerHead.Position).magnitude / 100
-                                        end
-                                        Closest[1] = DistanceFromCenter
-                                        local PlayerAim = nil
-                                        if AimPart == "Torso" then
-                                            PlayerAim = v.Character:FindFirstChild("HumanoidRootPart") or v.Character:FindFirstChild("Torso")
-                                        else
-                                            PlayerAim = v.Character.Head
-                                        end
-                                        Closest[2] = PlayerAim
-                                        Closest[3] = Vector2.new(PlayerScreen.X, PlayerScreen.Y)
-                                        Closest[4] = Prediction
-                                        Closest[5] = v
-                                    end
-                                end
-                            end)
+                if Obscuring == false and ((Closest[1] ~= nil and DistanceFromCenter < Closest[1]) or Closest[1] == nil) then
+                if Closest[1] == nil or (DistanceFromCenter < Closest[1]) then
+                local Prediction = Vector3.new(0, 0, 0)
+                if MovementPrediction == true then
+                Prediction = PlayerRoot.Velocity * (MovementPredictionStrength / 10) * (Client.Character.Head.Position - PlayerHead.Position).magnitude / 100
+             end
+                Closest[1] = DistanceFromCenter
+                local PlayerAim = nil
+                if AimPart == "Torso" then
+                PlayerAim = v.Character:FindFirstChild("HumanoidRootPart") or v.Character:FindFirstChild("Torso")
+                else
+                PlayerAim = v.Character.Head
+             end
+          Closest[2] = PlayerAim
+          Closest[3] = Vector2.new(PlayerScreen.X, PlayerScreen.Y)
+          Closest[4] = Prediction
+          Closest[5] = v
+       end
+    end
+end)
+
 return aimbot
